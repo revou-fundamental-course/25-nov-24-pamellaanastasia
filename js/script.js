2bbebd162ec493be6700e3cdeec33089e8636a02
pamellaanastasia <pamella.anastasia@gmail.com>
2024-12-05 22:16:31 +0800

    js 1st update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// JAVASCRIPT PAGE
+// Form Validation & BMI Calculation Variables
+const weight = document.getElementById('inputweight').value;
+const height = document.getElementById('inputheight').value;
+const age = document.getElementById('inputage').value;
+
+console.log('Weight' + weight);
+console.log('Height' + height);
+console.log('Age' + age);
+
+// Form Validation
+function validate() {   
+    if (weight != '' && height != '' && age != '') {
+        calculate();
+    } else {
+        alert('Please fill in all field first!');
+    }
+}
